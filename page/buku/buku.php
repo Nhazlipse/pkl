25655dbfe8ce7c2c5b349db108e0f86058b88e9f
Nhazlipse <nhazlipse@gmail.com>
2023-03-23 02:18:55 +0700

    anas

--- page/buku/buku.php
+++ page/buku/buku.php
@@ -12,45 +12,27 @@
                                 <table class="table table-striped table-bordered table-hover" id="dataTables-example">
                                     <thead>
                                         <tr>
-                                            <th># </th>
-                                            <th>Judul</th>
-                                            <th>Pengarang</th>
-                                            <th>Penerbit</th>
-                                            <th>Tahun Terbit</th>
-                                            <th>Jumlah Buku</th>
-                                            <!--<th>ISBN</th>-->
-                                            <th>Gambar</th>
-                                            <th>Aksi</th>
+                                            <th>No</th>
+                                            <th>Nama Perusahaan</th>
+                                            <th>Alamat Perusahaan</th>
+                                            <th>No Fax</th>
+                                            <th>No Telp</th>
+                                            <th>Alamat Email</th>
+                                            <th>Direktur</th>
+                                            <th>No Rekening</th>
+                                            <th>Bank</th>
+                                            <th>Kantor Bank</th>
+                                            <th>Atas Nama</th>
+                                            <th>Ket Akta Pendirian</th>
+                                            <th>No DPT</th>
+                                            <th>No SAPV</th>
+                                            <th>No NPWP</th>
+                                            <th>Action</th>
                                         </tr>
                                     </thead>
-                                    <tbody>
-                                        
-                                        <?php
-                                            $buku = $mysqli->query("SELECT * FROM tb_buku ");
-                                            $no = 0;
-                                            while( $b = $buku->fetch_assoc()){
-                                            $no++;
-                                         ?>
-
-                                        <tr>
-                                            <th scope="row"><?= $no; ?></th>
-                                            <td><?= $b["judul"]; ?></td>
-                                            <td><?= $b["pengarang"]; ?></td>
-                                            <td><?= $b["penerbit"]; ?></td>
-                                            <td><?= $b["tahun_terbit"]; ?></td>
-                                            <td><?= $b["jumlah_buku"]; ?></td>
-                                            <!--<td><?= $b["isbn"]; ?></td>-->
-                                            <td><img src="images/buku/<?= $b["gambar"]; ?>" height="100"></td>
-                                            <td>
-                                                <a href="?page=buku&aksi=edit&id=<?php echo $b['id'];?>" class="btn btn-info">Edit </a>
-                                                <a href="?page=buku&aksi=delete&id=<?php echo $b['id'];?>" class="btn btn-danger" onclick="return confirm('Anda yakin mau menghapus data ini?');">Hapus </a>
-                                            </td>
-                                          <?php } ?>
-                                        </tr>
-                                    </tbody>
-                                </table>
-
-                                 </div>
+                                    <?php require 'panggil-datavendor.php';?>
+</table>
+</div>
                             <a href="./laporan/laporan_buku_exel.php" class="btn btn-default" target="blank" style="margin-top: 10px; "><i class="fa fa-print" style="margin-right: 7px;"> </i>ExportToExcel</a>
 
                             </div>
